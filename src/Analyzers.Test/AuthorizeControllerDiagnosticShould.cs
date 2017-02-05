b37b5ab9545f36fb254e92c79dd8a718df22f5c7
Justin Self <justinself@outlook.com>
2017-02-05 11:22:59 -0600

    Adds AuthorizeControllerDiagnostic with single unit test

---
+++ src/Analyzers.Test/AuthorizeControllerDiagnosticShould.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+using TestHelper;
+
+namespace Analyzers.Test
+{
+    [TestFixture]
+    public class AuthorizeControllerDiagnosticShould : CodeFixVerifier
+    {
+        [Test]
+        public void WarnIfNoAttributeIsFound()
+        {
+            var source = @"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = "Analyzers",
+                Message = "HomeController has no authorization attribute.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 9, 5)
+                    }
+            };
+
+            VerifyCSharpDiagnostic(source, expected);
+        }
+
+      
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AuthorizeControllerDiagnostic();
+        }
+    }
+}
