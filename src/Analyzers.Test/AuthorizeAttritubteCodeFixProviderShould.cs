7d312aacd9f076a8ce15a091b354a4ed5e615d05
Justin Self <justinself@outlook.com>
2017-02-05 13:59:57 -0600

    Adds a codefix for adding Authorize Attribute

---
+++ src/Analyzers.Test/AuthorizeAttritubteCodeFixProviderShould.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+using TestHelper;
+
+namespace Analyzers.Test
+{
+    [TestFixture]
+    public class AuthorizeAttritubteCodeFixProviderShould : CodeFixVerifier
+    {
+        [Test]
+        public void AddAuthorizeAttributeWhenNoOtherAttributesExist()
+        {
+            var source = @"using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+    }
+}";
+            var result = @"using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+    }
+}";
+            VerifyCSharpFix(source, result, null, true);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new AuthorizeControllerCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AuthorizeControllerDiagnostic();
+        }
+    }
+}
---
+++ src/Analyzers/Analyzers/AuthorizeControllerCodeFixProvider.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace Analyzers
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AnalyzersCodeFixProvider)), Shared]
+    public class AuthorizeControllerCodeFixProvider : CodeFixProvider
+    {
+        private const string title = "Add Authorize Attribute";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(AuthorizeControllerDiagnostic.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var classDeclarationSyntax =
+                root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ClassDeclarationSyntax>().First();
+
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: title,
+                    createChangedSolution: c => AddAuthorizeAttribute(context.Document, classDeclarationSyntax, root, c),
+                    equivalenceKey: title),
+                diagnostic);
+        }
+
+        private Task<Solution> AddAuthorizeAttribute(Document document,
+            ClassDeclarationSyntax classDeclarationSyntax, SyntaxNode root,
+            CancellationToken cancellationToken)
+        {
+
+            var attributeSyntax = SyntaxFactory.Attribute(SyntaxFactory.IdentifierName("Authorize"));
+            var attributeListSyntax = SyntaxFactory.AttributeList(SyntaxFactory.SingletonSeparatedList(attributeSyntax));
+            var attributeLists = classDeclarationSyntax.AttributeLists.Add(attributeListSyntax.NormalizeWhitespace());
+
+            var newRootWithAuthorizeAttribute = root.ReplaceNode(classDeclarationSyntax,
+                classDeclarationSyntax.WithAttributeLists(attributeLists));
+
+            var aspNetCoreQualifiedNames = SyntaxFactory.QualifiedName(
+                SyntaxFactory.IdentifierName("Microsoft"), SyntaxFactory.IdentifierName("AspNetCore"));
+
+            var authorizeAttributeQualifiedName = SyntaxFactory.QualifiedName(aspNetCoreQualifiedNames,
+                SyntaxFactory.IdentifierName("Authorization"));
+
+            var authorizeUsingDirective = SyntaxFactory.UsingDirective(authorizeAttributeQualifiedName);
+
+            var newRootWithUsingDirective = newRootWithAuthorizeAttribute.InsertNodesBefore(
+                newRootWithAuthorizeAttribute.ChildNodes().OfType<UsingDirectiveSyntax>().First(),
+                new[] {authorizeUsingDirective});
+
+            var formattedRoot = Formatter.Format(newRootWithUsingDirective, new AdhocWorkspace());
+
+            var newSolution = document.WithSyntaxRoot(formattedRoot).Project.Solution;
+
+            return Task.FromResult(newSolution);
+        }
+    }
+}
